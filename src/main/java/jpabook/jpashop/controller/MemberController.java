ebd20f1372ae6f70d78d3a732ed8e9978165db2a
SeongbinPark <binni0515@gmail.com>
2022-08-13 17:32:19 +0900

    7-1,2. 홈화면레이아웃, 회원가입(간단한 thymeleaf, controller, @Valid, BindingResult)

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.controller;
+
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")//폼을 열어본다.
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        //Model: 컨트롤러에서 뷰를 넘어갈때 map으로 데이터 싣어서 넘김.
+        //여기선 MemberForm 객체를 넘겼으므로 화면에서 이 객체에 접근 할 수 있게됨.
+        return "members/createMemberForm";//resources/templates/ 이후 경로.
+    }
+
+    //create에서 왜 Member(엔티티)로 안받고 MemberForm으로 받나? -> 차이가 크다.
+    @PostMapping("/members/new")//실제 데이터를 등록한다.
+    public String create(@Valid MemberForm form, BindingResult result) {
+        //만약 form에 오류가 있으면 팅겨버리는데(컨트롤러에 코드가 안넘어감) 근데
+        //하지만 뭔가를 @Valid 하고 뒤에 BindingResult가 있으면
+        // valid 실패시 오류가 result에 담겨서 컨트롤러코드가 실행이됨.
+
+        if (result.hasErrors()) {//여기서의 에러는 Name을 입력 안했을 때만!!!!!!!!!!!!!
+            return "members/createMemberForm";//스프링이 이 화면까지 result를 끌고간다.
+            //그래서 어떤 에러가 있는지 이 화면에서 뿌릴 수 있다.
+        }
+
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member=new Member();
+        member.setAddress(address);
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";//등록을하고나면 재로딩되면 안좋아서 리다이렉트로 첫번째 페이지로 넘어감.
+    }
+}
---
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.controller;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class MemberForm {
+
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
