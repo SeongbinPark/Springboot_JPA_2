a84514d009ef5264873789448f58fdec1d75a191
SeongbinPark <binni0515@gmail.com>
2022-08-13 15:40:02 +0900

    6-5. 주문검색기능 개발(동적쿼리 처리방법)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,15 @@
 package jpabook.jpashop.repository;
 
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,10 +26,75 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //주문 검색기능( 회원명, orderstatus로 ) 은 추후에 추가.
+    /**
+     * 동적쿼리를 스트링의 합으로 작성 ( 권장방법 X )
+     */
+    //주문 검색기능( 회원명, orderstatus로 )j
+    public List<Order> findAll(OrderSearch orderSearch) {
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
 
-}
+        //주문 상태 검색(status 나 name이 있을 때 조건 추가) = 동적 쿼리
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";//조건여러개부터는 where이 아닌 and로 잇는다.
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+    }
+
+    /**
+     * JPA Criteria 로 해결 ( 권장방법 X )
+     * 유지보수 X : 딱 보면 sql이 안떠오른다.
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name =
+                    cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));     TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
+        return query.getResultList(); }
+    }
+//-> 해결법 : 쿼리 DSL(동적쿼리에 강력한 기능, 정적쿼리도 길어지면 DSL)
---
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.repository;
+
+
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+
+    private String memberName;//회원이름
+    private OrderStatus orderStatus;//주문 상태 [ORDER, CANCEL]
+    // 이 두가지 조건 중 파라미터 조건이 있으면 WHERE로 검색
+
+
+}
