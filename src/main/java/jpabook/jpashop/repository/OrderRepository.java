c47654a910740b42fbf9fffdeec16863ca99ce7e
SeongbinPark <binni0515@gmail.com>
2022-08-13 11:24:28 +0900

    6-2. OrderRepository 개발(검색기능은 추후추가)

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    //주문 검색기능( 회원명, orderstatus로 ) 은 추후에 추가.
+
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//    }
+
+}
