a4a03ccc217845327e3faeaa45828b28d8ad5efb
SeongbinPark <binni0515@gmail.com>
2022-09-08 23:15:02 +0900

    1-1. 회원등록API(컨트롤러에서는 DTO로 받자.)

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -4,6 +4,7 @@ package jpabook.jpashop.api;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,8 +23,27 @@ public class MemberApiController {
 
     @PostMapping("/api/v1/members") // 회원 등록 API // @RequestBody는 JSON으로 온 req를 Member에 파싱해서 넣어줌.
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) { //물론 나중에 DTO로 바꿈
+        Long id = memberService.join(member); // @Valid 어겼을 때 예외 처리 -> ControllerAdvice 사용.
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+//        Member member = Member.builder()
+//                .id(request.getId())
+//                .name(request.getName())
+//                .build();
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
     }
 
     @Data
@@ -33,8 +54,6 @@ public class MemberApiController {
             this.id = id;
         }
     }
-
-
 
 
 }
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,12 +1,10 @@
 package jpabook.jpashop.domain;
 
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +13,7 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class Member {
 
     @Id
@@ -22,7 +21,6 @@ public class Member {
     @Column(name = "member_id")//칼럼 이름 지정.
     private Long id;
 
-    @NotNull
     private String name;
 
     @Embedded
@@ -33,4 +31,11 @@ public class Member {
     //==난 읽기전용이다., 난 매핑된 거울일 뿐이야.
     private List<Order> orders = new ArrayList<>();
     //여기에 무슨 값을 넣는다 해서 저 FK값이 변경되지 않는다.
+
+
+    @Builder
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
