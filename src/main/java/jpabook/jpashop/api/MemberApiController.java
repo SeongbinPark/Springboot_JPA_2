e63ac02d995baa76f519c4de85a009b9f83df8b6
SeongbinPark <binni0515@gmail.com>
2022-09-09 00:38:35 +0900

    1-3. 회원조회API(파라미터, 반환에는 엔티티절대금지, 모두 DTO로 쓰자., JSON구조 지키기 위해 Result로 한 번 감싸서 반환)

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,9 +21,45 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    /**
+     * 회원 조회 API
+     */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers(); //@RestController에 @ResponseBody 있으므로 리스트가 JSON으로 변환된다.
+    }
 
+    @GetMapping("api/v2/members")
+    public Result memberV2() {
+        List<MemberDto> collect = memberService.findMembers().stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(),collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    //컬렉션을 바로 반환하면 JSON 배열타입(전체가 [ ]로 감싸진 구조)으로 나가기 때문에 Result로 한 번 감싸서 반환. (배열로 나가면 유연성 별로)
+    // -> 추후 요구사항 분명히 들어온다. -> JSON 구조 안깨지면서 추가하려면 한번 감싸줘야함.
+    static class Result<T> { // 이때 T 는 List<MemberDto>
+        private int count; // 이렇게 필드만 추가하면 간단히
+        private T data; //이때 data는 List<MemberDto>
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name; // 요구사항이 이름만 넘기는 걸로 하자.
+    }
+
+
+
+    /**
+     * 회원 가입, 수정 API
+     */
     @PostMapping("/api/v1/members") // 회원 등록 API // @RequestBody는 JSON으로 온 req를 Member에 파싱해서 넣어줌.
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) { //물론 나중에 DTO로 바꿈
+
         Long id = memberService.join(member); // @Valid 어겼을 때 예외 처리 -> ControllerAdvice 사용.
         return new CreateMemberResponse(id);
     }
@@ -49,6 +87,8 @@ public class MemberApiController {
 
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
+
+
 
 
     @Data
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -29,6 +30,7 @@ public class Member {
     @OneToMany(mappedBy = "member")//Member:List<Order>=one:many
     //orders 테이블에 있는 member 필드에 의해 난 매핑된거야.
     //==난 읽기전용이다., 난 매핑된 거울일 뿐이야.
+    @JsonIgnore
     private List<Order> orders = new ArrayList<>();
     //여기에 무슨 값을 넣는다 해서 저 FK값이 변경되지 않는다.
 
